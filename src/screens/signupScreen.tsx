cb72599e144db8e971f06cf74ecaf23c57b562a5
Rahul Chaurasiya <rahul79990444@gmail.com>
2025-10-01 15:55:50 +0530

    feat: add mock api call while signup and login

--- src/screens/signupScreen.tsx
+++ src/screens/signupScreen.tsx
@@ -1,10 +1,12 @@
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native'
+import { StyleSheet, Text, View, TextInput } from 'react-native'
 import React from 'react'
 import { Formik } from 'formik'
 import * as Yup from 'yup';
 import { storage } from '../services/storage';
 import { useDispatch } from 'react-redux';
 import signupData from '../mocks/signup.json'
+import { api } from '../services/api';
+import CustomButton from '../components/button';
 
 const SignupScreen = ({ navigation }: any) => {
 
@@ -16,11 +18,25 @@ const SignupScreen = ({ navigation }: any) => {
         password: Yup.string().required('Required'),
     })
 
-    const handleSignup = (values: any) => {
-        console.log('signup data:', signupData);
-        dispatch({ type: 'auth/login', payload: { user: { name: values.name, email: values.email } } });
-        storage.set('auth', JSON.stringify({ user: { name: values.name, email: values.email } }));
-        console.log('signup successfull:', values);
+    const handleSignup = async (values: any) => {
+        try {
+
+            console.log('signup data:', signupData);
+
+            // Simulate Mock API call
+            const response = await api.signup();
+            console.log("API response:", response);
+
+            // update redux state
+            dispatch({ type: 'auth/login', payload: { user: { name: values.name, email: values.email } } });
+
+            // save session in mmkv
+            storage.set('auth', JSON.stringify({ user: { name: values.name, email: values.email } }));
+            
+            console.log('signup successfull:', values);
+        } catch (error) {
+            console.error('Signup error:', error);
+        }
     }
 
     return (
@@ -64,7 +80,7 @@ const SignupScreen = ({ navigation }: any) => {
                         />
                         {touched.password && errors.password && <Text style={styles.error}>{errors.password}</Text>}
 
-                        <Button title='Sign Up' onPress={handleSubmit} />
+                        <CustomButton text="Signup" onPress={handleSubmit} />
 
                         <Text style={styles.loginText} onPress={() => navigation.navigate('Login')}>Already have an account? Login</Text>
                     </>
@@ -77,8 +93,8 @@ const SignupScreen = ({ navigation }: any) => {
 export default SignupScreen
 
 const styles = StyleSheet.create({
-    container: { flex: 1, padding: 20, justifyContent: 'center'},
-    title: { fontSize: 24, fontWeight: 'bold', marginBottom: 20, textAlign: 'center'},
+    container: { flex: 1, padding: 20, justifyContent: 'center' },
+    title: { fontSize: 24, fontWeight: 'bold', marginBottom: 20, textAlign: 'center' },
     input: { borderWidth: 1, padding: 10, marginBottom: 10, borderRadius: 5, color: 'black' },
     error: { color: 'red', marginBottom: 10 },
     loginText: { color: 'blue', marginTop: 10, textAlign: 'center' },
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,20 @@
+// export const api = {
+//     login: async () => Promise.resolve(require("../mocks/login.json")),
+//     signup: async () => Promise.resolve(require("../mocks/signup.json")),
+//     getCars: async () => Promise.resolve(require("../mocks/cars.json")),
+//     getLocations: async () => Promise.resolve(require("../mocks/locations.json")),
+//     getBookings: async () => Promise.resolve(require("../mocks/bookings.json")),
+//   };
+
+const delay = (ms: number) => new Promise<void>(res => setTimeout(res, ms));
+
 export const api = {
-    login: async () => Promise.resolve(require("../mocks/login.json")),
-    signup: async () => Promise.resolve(require("../mocks/signup.json")),
-    getCars: async () => Promise.resolve(require("../mocks/cars.json")),
-    getLocations: async () => Promise.resolve(require("../mocks/locations.json")),
-    getBookings: async () => Promise.resolve(require("../mocks/bookings.json")),
-  };
+  login: async () => {
+    await delay(3000);
+    return require("../mocks/login.json");
+  },
+  signup: async () => {
+    await delay(3000);
+    return require("../mocks/signup.json");
+  } 
+};
